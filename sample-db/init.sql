e4784b7ac2ba30e487918ba88a38683f710052df
Muramatsu <62125008+Glider2355@users.noreply.github.com>
2024-09-01 18:54:04 +0900

    DBを含めたテスト追加

--- sample-db/init.sql
+++ sample-db/init.sql
@@ -5,4 +5,4 @@ CREATE TABLE Users (
   nameKana varchar(50) COMMENT '名前(カナ)',
   dateOfBirth date COMMENT '生年月日',
   PRIMARY KEY (userId)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザテーブル';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='ユーザテーブル';
